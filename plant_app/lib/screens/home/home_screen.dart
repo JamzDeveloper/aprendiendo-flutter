b71af9c2fa10bef8d07442c632de14c68c857b37
JamzDeveloper <JamzDeveloper@gmail.com>
2022-06-11 07:21:19 -0500

    mejoramiento de plant app

--- plant_app/lib/screens/home/home_screen.dart
+++ plant_app/lib/screens/home/home_screen.dart
@@ -1,24 +1,167 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart' as FlutterSVG;
-import 'package:plant_app/components/my_buttom_appbar.dart';
+import 'package:plant_app/constants.dart';
+// import 'package:plant_app/components/my_buttom_appbar.dart';
+import 'package:plant_app/screens/details/details_screen.dart';
 
 import 'package:plant_app/screens/home/components/body.dart';
+import 'package:plant_app/screens/perfil/perfil_screen.dart';
+import 'package:plant_app/screens/process_images/process_images_screen.dart';
 
-class HomeScren extends StatelessWidget {
-  const HomeScren({Key? key}) : super(key: key);
+class HomeScren extends StatefulWidget {
+  @override
+  State<HomeScren> createState() => _HomeScrenState();
+}
+
+class _HomeScrenState extends State<HomeScren> {
+  int currentTab = 0;
+
+  final List<Widget> screens = [
+    PerfilScreen(),
+    ProcessImageScreen(),
+    DetailsScreen()
+  ];
+  final PageStorageBucket bucket = PageStorageBucket();
+  Widget currentScreen = BodyHome();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       appBar: buildAppBar(),
-      body: BodyHome(),
-      bottomNavigationBar: MyBottomNavBar(),
+      body: PageStorage(
+        child: currentScreen,
+        bucket: bucket,
+      ),
+      // bottomNavigationBar: MyBottomNavBar(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 8,
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = BodyHome();
+                        currentTab = 0;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.home,
+                            color:
+                                currentTab == 0 ? kPrimaryColor : Colors.grey),
+                        Text(
+                          "Home",
+                          style: TextStyle(
+                              color: currentTab == 0
+                                  ? kPrimaryColor
+                                  : Colors.grey),
+                        )
+                      ],
+                    ),
+                  ),
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = DetailsScreen();
+                        currentTab = 1;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.dashboard_outlined,
+                            color:
+                                currentTab == 1 ? kPrimaryColor : Colors.grey),
+                        Text(
+                          "Detail",
+                          style: TextStyle(
+                              color: currentTab == 1
+                                  ? kPrimaryColor
+                                  : Colors.grey),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = PerfilScreen();
+                        currentTab = 2;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.account_circle_rounded,
+                            color:
+                            currentTab == 2 ? kPrimaryColor : Colors.grey),
+                        Text(
+                          "Detail",
+                          style: TextStyle(
+                              color: currentTab == 2
+                                  ? kPrimaryColor
+                                  : Colors.grey),
+                        )
+                      ],
+                    ),
+                  ),
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = PerfilScreen();
+                        currentTab = 3;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.account_circle_rounded,
+                            color:
+                                currentTab == 3 ? kPrimaryColor : Colors.grey),
+                        Text(
+                          "Detail",
+                          style: TextStyle(
+                              color: currentTab == 3
+                                  ? kPrimaryColor
+                                  : Colors.grey),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 
   AppBar buildAppBar() {
     return AppBar(
-
       elevation: 0,
       leading: IconButton(
         icon: FlutterSVG.SvgPicture.asset("assets/icons/menu.svg"),
---
+++ plant_app/lib/screens/process_images/process_images_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProcessImageScreen extends StatelessWidget {
+  const ProcessImageScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
